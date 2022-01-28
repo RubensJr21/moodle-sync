d488a00e259ffa578fc20f9352dd9d6ca76ac497
RubensJr21 <junior.correa2001@gmail.com>
2022-01-28 15:13:08 -0300

    Test API

---
+++ pages/api/getToken.ts
@@ -0,0 +1,46 @@
+import axios, {AxiosResponse} from 'axios';
+import {NextApiRequest, NextApiResponse} from 'next'
+
+interface tokenResponse{
+    token: string;
+}
+
+interface objectQuery {
+    domainBase: string,
+    username: string,
+    password: string,
+    service: string,
+}
+
+export default async function getToken(request: NextApiRequest, response: NextApiResponse) {
+
+
+    const {
+        domainBase,
+        username,
+        password,
+        service
+    }: objectQuery = request.body as unknown as objectQuery
+
+
+    /* 
+        domainBase = <domainBase>
+        username = <username>
+        password = <password>
+        service = moodle_mobile_app
+
+    */
+
+    // const {data}: AxiosResponse = await axios.get(`https://${domainBase}?username=${username}&password=${password}&service=${service}`)
+    // console.log(data)
+    // const {token}: tokenResponse = await data.json();
+    
+    response.json({
+        result: {
+            domainBase,
+            username,
+            password,
+            service
+        }
+    })
+}
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,3 +1,41 @@
 export default function Home(){
-    return <div>Home</div>
+    return <>
+        <fieldset className="dataUser">
+            <legend>Dados de login</legend>
+            <div id="dominio">
+                <label htmlFor="dominio">Domínio: </label>
+                <input type="text" name="dominio"/>
+            </div>
+            <div id="matricula">
+                <label htmlFor="matricula">Matrícula: </label>
+                <input type="text" name="matricula"/>
+            </div>
+            <div id="password">
+                <label htmlFor="senha">Senha: </label>
+                <input type="password" name="password"/>
+            </div>
+            <button type="submit" onClick={async () => {
+                const dominio = (document.querySelector("#dominio > input") as HTMLInputElement).value
+                const matricula = (document.querySelector("#matricula > input") as HTMLInputElement).value
+                const password = (document.querySelector("#password > input") as HTMLInputElement).value               
+
+                const result: Response = await fetch('/api/getToken', {
+                    method: 'POST',
+                    headers: {
+                        'Accept': 'application/json',
+                        'Content-Type': 'application/json'
+                    },
+                    body: JSON.stringify({
+                        domainBase : dominio,
+                        username: matricula,
+                        password,
+                        service: "moodle_mobile_app",
+                    })
+                });
+                console.log(result)
+                const j: Object = await result.json()
+                console.log(j)
+            }}>Sincronizar</button>
+        </fieldset>
+    </>
 }
